019750f6f9cca7b642c63bae4c02f61a8261e79e
caeriks0412 <caeriks@student.chalmers.se>
2021-11-03 15:56:49 +0100

    double

--- src/TestFilNummerInf.java
+++ src/TestFilNummerInf.java
@@ -3,7 +3,7 @@ public class TestFilNummerInf {
 
 	public static void main(String[] args) {
 		System.out.println("wooooooooooooooooooooooooo");
-		
+		System.out.println("owwwwwwwwwwwwwwwwwwwwwwwww");
 
 	}
 	// adsbaoidbaidnalnd
