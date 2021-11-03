42d87f5a536e7b82e132af7483cfa8313c71e281
joelgrasjostudent <grasjo@student.chalmers.se>
2021-11-03 15:56:45 +0100

    hhe

--- src/TestFilNummerInf.java
+++ src/TestFilNummerInf.java
@@ -2,10 +2,12 @@
 public class TestFilNummerInf {
 
 	public static void main(String[] args) {
-		System.out.println("wooooooooooooooooooooooooo");
+		System.out.println("woooooooooo0ooooooooooooooo");
 		
 
 	}
 	// adsbaoidbaidnalnd
 	//Hej
+	
+	// 123v ajfnisdabisdbahsbfshdbfhsbdksjd
 }
